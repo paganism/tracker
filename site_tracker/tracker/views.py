9d8c3bcd7ed030bc1514657f7cf784555c0099db
Yuriy Belov <paganism@mail.ru>
2020-06-24 21:51:52 +0300

    delete debugging prints

--- site_tracker/tracker/views.py
+++ site_tracker/tracker/views.py
@@ -43,14 +43,11 @@ def login(request):
     if username is None or password is None:
         return Response({'error': 'Please provide both username and password'},
                         status=HTTP_400_BAD_REQUEST)
-    print(password)
     user = authenticate(username=username, password=password)
     if not user:
         return Response({'error': 'Invalid Credentials'},
                         status=HTTP_404_NOT_FOUND)
     token, _ = Token.objects.get_or_create(user=user)
-    print(user, user.is_authenticated)
-    print(token)
     return Response({'token': token.key},
                     status=HTTP_200_OK)
 
